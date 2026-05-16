916ae53cd478e3ba0d46bce13715f681addb149a
Echo-73 <divijabellapukonda@gmail.com>
2026-05-16 11:59:30 +0530

    Initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,69 @@
+import React, { useState } from 'react'
+import Sidebar from './components/Sidebar'
+import { Route, Routes, useLocation } from 'react-router-dom'
+import ChatBox from './components/ChatBox'
+import Credits from './pages/Credits'
+import Community from './pages/Community'
+import { assets } from './assets/assets'
+import './assets/prism.css'
+import Loading from './pages/Loading'
+import Login from './pages/Login'
+import { useAppContext } from './context/AppContext'
+
+const App = () => {
+
+  const { user } = useAppContext()
+
+  const [isMenuOpen, setIsMenuOpen] = useState(true)
+
+  const { pathname } = useLocation()
+
+  if (pathname === '/loading') return <Loading />
+
+  return (
+    <>
+
+      {/* Mobile Menu Open Button */}
+      {!isMenuOpen && (
+        <img
+          onClick={() => setIsMenuOpen(true)}
+          src={assets.menu_icon}
+          alt=""
+          className='md:hidden absolute top-4 left-4 z-50 w-6 cursor-pointer not-dark:invert'
+        />
+      )}
+
+      {user ? (
+
+        <div className='dark:bg-gradient-to-b from-[#242124] to-[#000000] dark:text-white'>
+
+          <div className='flex h-screen w-screen'>
+
+            <Sidebar
+              isMenuOpen={isMenuOpen}
+              setIsMenuOpen={setIsMenuOpen}
+            />
+
+            <Routes>
+              <Route path='/' element={<ChatBox />} />
+              <Route path='/credits' element={<Credits />} />
+              <Route path='/community' element={<Community />} />
+            </Routes>
+
+          </div>
+
+        </div>
+
+      ) : (
+
+        <div className='bg-gradient-to-b from-[#242124] to-[#000000] flex items-center justify-center h-screen w-screen'>
+          <Login />
+        </div>
+
+      )}
+
+    </>
+  )
+}
+
+export default App
---
+++ server/configs/db.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+    try {
+
+        await mongoose.connect(process.env.MONGODB_URI, {
+            dbName: "Nabiai"
+        });
+
+        console.log("Database Connected");
+
+    } catch (error) {
+        console.log("MongoDB Error:", error.message);
+    }
+};
+
+export default connectDB;
